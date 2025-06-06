5859a14dc44f7f5bb4e2dc3f7a11047f69a93fcd
Andrey <Raikov1998@mail.ru>
2025-06-06 18:48:49 +0300

    refactoring, add .gitignore

--- tests/test_pokemon.py
+++ tests/test_pokemon.py
@@ -1,5 +1,7 @@
+"""
+Test example
+"""
 import requests
-import pytest
 
 URL = 'https://api.pokemonbattle.ru/v2'
 TOKEN = '7afcd330135ee444af0b11f9ac4396ae'
@@ -7,9 +9,19 @@ HEADER = {'Content-Type':'application/json', 'trainer_token':TOKEN}
 TRAINER_ID = '33488'
 
 def test_status_code():
-   response = requests.get(url = f'{URL}/pokemons', params = {'trainer_id' : TRAINER_ID})
-   assert response.status_code == 200
+    """
+    GT-1. Check status code
+    """
+    response = requests.get(url = f'{URL}/pokemons', params = {'trainer_id' : TRAINER_ID},
+                            timeout=5)
+    assert response.status_code == 200, "Unexpected status code"
 
 def test_trainer_name():
-   response_get = requests.get(url = f'{URL}/me', headers = HEADER, params = {'trainer_id' : TRAINER_ID})
-   assert response_get.json()["data"][0]['trainer_name'] == 'Grand master byte'
+    """
+    GT-2. Check trainer name
+    """
+    response_get = requests.get(url = f'{URL}/me', headers = HEADER, 
+                                params = {'trainer_id' : TRAINER_ID},
+                                timeout=5)
+    assert response_get.json()["data"][0]['trainer_name'] == 'Grand master byte', \
+        "Unexpected value for trainer_name"
